4a15fab2df69abc831d85792698cb0e34445373e
Tyler Chin <tylerstyle05@gmail.com>
2026-06-04 16:32:36 -0700

    removed link

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import CardDoorTop from "../assets/CardDoorTop.png";
 import CardDoorBottom from "../assets/CardDoorBottom.png";
