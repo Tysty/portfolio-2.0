54627743228700152c14e5052e24e812ffef7a9c
Tyler Chin <tylerstyle05@gmail.com>
2026-06-04 17:28:25 -0700

    Added resume page

--- src/resume.tsx
+++ src/resume.tsx
@@ -1,4 +1,16 @@
 function resume() {
-  return <div>Resume Page</div>;
+  return (
+    <>
+      <h1 className="center home-title">Resume</h1>
+      <br />
+      <iframe
+        src="https://rose-lauretta-19.tiiny.site"
+        width="1000"
+        height="1100"
+        allowFullScreen
+        style={{ display: "block", margin: "0 auto" }}
+      />
+    </>
+  );
 }
 export default resume;
